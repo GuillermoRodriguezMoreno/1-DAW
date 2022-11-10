973c0571e23c239502de424314d703092feadf9f
GuillermoRodriguezMoreno <117939862+GuillermoRodriguezMoreno@users.noreply.github.com>
2022-11-10 20:17:21 +0100

    pos si

--- 1-DAW(VS)/PROGRAMACION/Examen/pruebagithub.java
+++ 1-DAW(VS)/PROGRAMACION/Examen/pruebagithub.java
@@ -3,6 +3,6 @@ package Examen;
 public class pruebagithub {
     public static void main(String[] args) {
         
-        System.out.println("hola desde pues ahora lo hago por ultima vez");
+        System.out.println("hola desde pues ahora lo hago por ultima vez y yasta");
     }
 }
