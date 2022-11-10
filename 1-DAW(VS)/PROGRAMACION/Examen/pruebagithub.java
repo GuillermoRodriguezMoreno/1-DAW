21f8cf8d20c298d590c9768ee04e4cffed2046bd
GuillermoRodriguezMoreno <117939862+GuillermoRodriguezMoreno@users.noreply.github.com>
2022-11-10 20:15:07 +0100

    hola desde github

--- 1-DAW(VS)/PROGRAMACION/Examen/pruebagithub.java
+++ 1-DAW(VS)/PROGRAMACION/Examen/pruebagithub.java
@@ -3,6 +3,6 @@ package Examen;
 public class pruebagithub {
     public static void main(String[] args) {
         
-        System.out.println("hola desde pc");
+        System.out.println("hola desde github");
     }
 }
