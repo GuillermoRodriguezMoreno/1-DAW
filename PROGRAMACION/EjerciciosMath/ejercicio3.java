af8c263d35f92ed3f0e1bacfdbbface1eb2ded92
GuillermoRodriguezMoreno <117939862+GuillermoRodriguezMoreno@users.noreply.github.com>
2022-11-10 19:45:50 +0100

    Delete PROGRAMACION directory

--- PROGRAMACION/EjerciciosMath/ejercicio3.java
+++
@@ -1,8 +0,0 @@
-package EjerciciosMath;
-
-public class ejercicio3 {
-    public static void main(String[] args) {
-        
-        System.out.println(Math.round(Math.random()));
-    }
-}
--- PROGRAMACION/EjerciciosMath/ejercicio6.java
+++
@@ -1,8 +0,0 @@
-package EjerciciosMath;
-
-public class ejercicio6 {
-    public static void main(String[] args) {
-        
-        System.out.println((int)(Math.floor(Math.PI)));
-    }
-}
